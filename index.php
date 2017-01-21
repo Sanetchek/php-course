21a7b60799a126a23c8bd17c2d23736efca64386
Aleksandr <algryshko@gmail.com>
2017-01-21 13:12:16 +0200

    lesson3

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
-<?php include('header.php'); ?>
+<?php require('header.php'); ?>
 
     <div class="lesson">
         <h2 class="icon-menu3"> Задание к уроку 1</h2>
         <div class="lesson-content">
-            <?php include('lesson1.php'); ?>
+            <?php require('lesson1.php'); ?>
         </div>
     </div>
     <div class="lesson">
         <h2 class="icon-menu3"> Задание к уроку 2</h2>
         <div class="lesson-content">
-            <?php include('lesson2.php'); ?>
+            <?php require('lesson2.php'); ?>
         </div>
     </div>
     <div class="lesson">
         <h2 class="icon-menu3"> Задание к уроку 3</h2>
         <div class="lesson-content">
-            <?php include('lesson3.php'); ?>
+            <?php require('lesson3.php'); ?>
         </div>
     </div>
 
-<?php include('footer.php'); ?>
+<?php require('footer.php'); ?>
